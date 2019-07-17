5d9e6c338d65e0f24f9e183a70e7f34d52b6c02b
halilmarx07 <halil_marx07@hotmail.com>
2019-07-17 17:34:12 +0300

    m

--- mailer.php
+++ mailer.php
@@ -22,7 +22,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "csealtaf@gmail.com";
+        $recipient = "halil_marx07@hotmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
